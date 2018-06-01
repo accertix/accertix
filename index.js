424515337c58e1215aec1c6aff035dc93428c6cf
Alvin Ofori <hagiosofori@gmail.com>
2018-06-01 13:37:55 +0000

    separating offices, lands, houses

--- index.js
+++ index.js
@@ -6,12 +6,27 @@
     const page = await browser.newPage();
     await page.goto("http://www.businessghana.com/site/real-estates");
     await page.waitFor(1000);
-
+    /**
+     * TODO: refactor to use less code, and be more readable
+     */
     const result = await page.evaluate(() => {
-      propertyLinks = document.querySelectorAll(".featured-image > a");
-      propertyLinks = Array.prototype.map.call(propertyLinks, function(e) {
+      allPropertyLinks = document.querySelectorAll(".featured-image > a");
+      allPropertyLinks = Array.prototype.map.call(allPropertyLinks, function(e) {
         return e.href;
       });
+      plotLinks = allPropertyLinks.filter(e => {
+        return e.startsWith("http://www.businessghana.com/site/real-estates/plots");
+      });
+      propertyLinks = allPropertyLinks.filter(e => {
+        return e.startsWith("http://www.businessghana.com/site/real-estates/properties");
+      });
+      shopLinks = allPropertyLinks.filter(e => {
+        return e.startsWith("http://www.businessghana.com/site/real-estates/shops");
+      });
+      officeLinks = allPropertyLinks.filter(e => {
+        return e.startsWith("http://www.businessghana.com/site/real-estates/offices");
+      });
+
       return {
         propertyLinks
         // price
@@ -24,67 +39,78 @@
   let getDetails = scrape().then(async links => {
     const browser = await puppeteer.launch({ headless: false });
     let data = [];
+    /**
+     * TODO: transfer these to a 'config' file, and import them for use.
+     *
+     */
+    
     const TITLE_SELECTOR = ".details-title > h2";
     const PRICE_SELECTOR =
-      "div.row.all-details-section > div > div:nth-child(4) > div > div:nth-child(3) > div.col-sm-9.col-sm-9.col-xs-8.section-info-content";
-  
-    const NUM_ROOMS_SELECTOR = "";
-    const NUM_BEDROOMS_SELECTOR = "";
-    const ACQUISITION_TYPE_SELECTOR = "";
-    const NUM_BATHROOMS_SELECTOR = "";
-    const PLOT_LENGTH_SELECTOR = "";
-    const PLOT_WIDTH_SELECTOR = "";
-    const PLOT_MEASURE_SELECTOR = "";
-    const DESCRIPTION_SELECTOR = "";
-    const LOCATION_SELECTOR = "";
-    const REGION_SELECTOR = "";
-    const STREET_ADDRESS_SELECTOR = "";
-    const LISTER_NAME_SELECTOR = "";
-    const LISTER_PHONE_SELECTOR = "";
-    const ADVERTISER_SELECTOR = "";
+      ".all-details-section > div > div:nth-child(4) > div > div:nth-child(3) > .section-info-content";
+    const NUM_ROOMS_SELECTOR =
+      ".all-details-section > div > div:nth-child(4) > div > div:nth-child(4) > .section-info-content";
+    const NUM_BEDROOMS_SELECTOR =
+      ".all-details-section > div > div:nth-child(4) > div > div:nth-child(5) > .section-info-content";
+    const ACQUISITION_TYPE_SELECTOR =
+      ".all-details-section > div > div:nth-child(4) > div > div:nth-child(6) > .section-info-content";
+    const NUM_BATHROOMS_SELECTOR =
+      ".all-details-section > div > div:nth-child(4) > div > div:nth-child(7) > .section-info-content";
+    const PLOT_LENGTH_SELECTOR =
+      ".all-details-section > div > div:nth-child(4) > div > div:nth-child(8) > .section-info-content";
+    const PLOT_WIDTH_SELECTOR =
+      ".all-details-section > div > div:nth-child(4) > div > div:nth-child(9) > .section-info-content";
+    const PLOT_MEASURE_SELECTOR =
+      ".all-details-section > div > div:nth-child(4) > div > div:nth-child(10) > .section-info-content";
+    const DESCRIPTION_SELECTOR =
+      ".all-details-section > div > div:nth-child(4) > div > div:nth-child(11) > .section-info-content";
+    const LOCATION_SELECTOR =
+      ".all-details-section > div > div:nth-child(5) > div > div:nth-child(3) > .section-info-content";
+    const REGION_SELECTOR =
+      ".all-details-section > div > div:nth-child(5) > div > div:nth-child(4) > .section-info-content";
+    const STREET_ADDRESS_SELECTOR =
+      ".all-details-section > div > div:nth-child(5) > div > div:nth-child(5) > .section-info-content";
+    const LISTER_NAME_SELECTOR =
+      ".all-details-section > div > div:nth-child(6) > div > div:nth-child(3) > .section-info-content";
+    const LISTER_PHONE_SELECTOR =
+      ".all-details-section > div > div:nth-child(6) > div > div:nth-child(4) > .section-info-content";
+    const ADVERTISER_SELECTOR =
+      ".all-details-section > div > div:nth-child(6) > div > div:nth-child(5) > .section-info-content";
 
-    
-    for (let i = 0; i < 3; i++) {
+    //TODO: limiting to 3, to manage memory for testing
+    //change to complete list in production.
+    //need to figure a better way to handle this.
+    for (let i = 0; i < 2; i++) {
       let page = await browser.newPage();
       await page.goto(links.propertyLinks[i]);
       await page.waitFor(3000);
 
       let result = await page.evaluate(() => {
         let title = document.querySelector(".details-title > h2").innerHTML;
-        let price = document.querySelector("div.row.all-details-section > div > div:nth-child(4) > div > div:nth-child(3) > div.col-sm-9.col-sm-9.col-xs-8.section-info-content").innerHTML;
-        return { title, price };
+        let price = document.querySelector(
+          "div.row.all-details-section > div > div:nth-child(4) > div > div:nth-child(3) > .section-info-content"
+        ).innerHTML;
+        let numRooms = document.querySelector(".all-details-section > div > div:nth-child(4) > div > div:nth-child(4) > .section-info-content").innerHTML;
+        let numBedrooms = document.querySelector(".all-details-section > div > div:nth-child(4) > div > div:nth-child(5) > .section-info-content").innerHTML;
+        let acquisitionType = document.querySelector(".all-details-section > div > div:nth-child(4) > div > div:nth-child(6) > .section-info-content").innerHTML;
+        let numBathrooms = document.querySelector(".all-details-section > div > div:nth-child(4) > div > div:nth-child(7) > .section-info-content").innerHTML;
+        let plotLength = document.querySelector(".all-details-section > div > div:nth-child(4) > div > div:nth-child(8) > .section-info-content").innerHTML;
+        let plotWidth = document.querySelector(".all-details-section > div > div:nth-child(4) > div > div:nth-child(9) > .section-info-content").innerHTML;
+        let plotMeasure = document.querySelector(".all-details-section > div > div:nth-child(4) > div > div:nth-child(10) > .section-info-content").innerHTML;
+        let desc = document.querySelector(".all-details-section > div > div:nth-child(4) > div > div:nth-child(11) > .section-info-content").innerHTML;
+        let location = document.querySelector(".all-details-section > div > div:nth-child(5) > div > div:nth-child(3) > .section-info-content").innerHTML;
+        let region = document.querySelector(".all-details-section > div > div:nth-child(5) > div > div:nth-child(4) > .section-info-content").innerHTML;
+        let streetAddress = document.querySelector(".all-details-section > div > div:nth-child(5) > div > div:nth-child(5) > .section-info-content").innerHTML;
+        let listerName = document.querySelector(".all-details-section > div > div:nth-child(6) > div > div:nth-child(4) > .section-info-content").innerHTML;
+        let listerPhone = document.querySelector(".all-details-section > div > div:nth-child(6) > div > div:nth-child(4) > .section-info-content").innerHTML;
+        let advertiser = document.querySelector(".all-details-section > div > div:nth-child(6) > div > div:nth-child(5) > .section-info-content").innerHTML;
+
+        return { title, price, numRooms, numBedrooms, numBathrooms, plotLength, plotWidth, plotMeasure, desc, location, region, streetAddress, listerName, listerPhone, advertiser };
       });
       data.push(result);
     }
-    browser.close();
+    // browser.close();
     return data;
-    // const data = await links.propertyLinks.map(async e => {
-    //     let page = await browser.newPage();
-    //     await page.goto(e);
-    //     await page.waitFor(1000);
-
-    //     const result = await page.evaluate(() => {
-    //         let title = document.querySelector(TITLE_SELECTOR);
-    //         let price = document.querySelector(PRICE_SELECTOR);
-    // let numRooms = document.querySelector(NUM_ROOMS_SELECTOR);
-    // let numBedrooms = document.querySelector(NUM_BEDROOMS_SELECTOR);
-    // let acquisitionType = document.querySelector(ACQUISITION_TYPE_SELECTOR);
-    // let bathrooms = document.querySelector(NUM_BATHROOMS_SELECTOR);
-    // let plotLength = document.querySelector(PLOT_LENGTH_SELECTOR);
-    // let plotWidth = document.querySelector(PLOT_WIDTH_SELECTOR);
-    // let plotMeasure = document.querySelector(PLOT_MEASURE_SELECTOR);
-    // let desc = document.querySelector(DESCRIPTION_SELECTOR);
-    // let location = document.querySelector(LOCATION_SELECTOR);
-    // let region = document.querySelector(REGION_SELECTOR);
-    // let streetAddress = document.querySelector(STREET_ADDRESS_SELECTOR);
-    // let listerName = document.querySelector(LISTER_NAME_SELECTOR);
-    // let listerPhone = document.querySelector(LISTER_PHONE_SELECTOR);
-    // let advertiser = document.querySelector(ADVERTISER_SELECTOR);
-
-    //     return {title, price};
-    // });
-    //     return result;
-    // })
+    
   });
 
   getDetails.then(result => {
